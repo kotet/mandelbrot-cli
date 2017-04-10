765693bffc89cbcbe7a1af2d717c6579e89cbc4d
Kotet <kotet@users.noreply.github.com>
2017-04-11 08:57:01 +0900

    Option parser

--- source/app.d
+++ source/app.d
@@ -1,6 +1,33 @@
-import std.stdio;
-
-void main()
+void main(string[] args)
 {
-	writeln("Edit source/app.d to start your project.");
+	import std.getopt : defaultGetoptPrinter, getopt, GetOptException,
+		GetoptResult;
+	import std.stdio : writeln;
+
+	real top = -1.0;
+	real left = -2.0;
+	real height = 2.0;
+	real width = 4.0;
+	size_t row = 20;
+	size_t column = 80;
+	size_t judge_iter = 20;
+
+	GetoptResult helpinfo;
+	try
+	{
+		helpinfo = getopt(args, "top", &top, "left", &left, "height",
+				&height, "width", &width, "row", &row, "column", &column,
+				"judge-iter", &judge_iter);
+	}
+	catch (Exception e)
+	{
+		writeln("Error: ", e.msg, "\nexit");
+		return;
+	}
+
+	if (helpinfo.helpWanted)
+		defaultGetoptPrinter("test", helpinfo.options);
 }
+
+string draw(real top, real left, real height, real width, size_t row,
+		size_t column, size_t judge_iter);
